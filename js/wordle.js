2e3e64dfe66ee693c87832f44013e80938e264a0
Sinuhe <sinuhenavarro@gmail.com>
2022-02-17 18:01:42 +0100

    refactorizadas algunas funciones

--- js/wordle.js
+++ js/wordle.js
@@ -11,14 +11,6 @@ var intentos = 0;
 //inputs con las letras escritas por el jugador
 var inputs;
 
-/*************************************************************
- * Oculta las casillas 1-5
-**************************************************************/
-function ocultarCasillas() {
-    for (let i = 1; i < 6; i++) {
-        $('#word' + i).css('display', 'none');
-    }
-}
 
 /*************************************************************
 * Muestra la siguiente fila de letras y bloquea la actual
@@ -33,12 +25,7 @@ function mostrarSiguientesCasillas() {
     $('#word' + intentos).children().eq(0).focus();
 }
 
-/*************************************************************
-* Comprueba cada letra y la colorea en verde si está en la posición correcta
-* y en amarillo si se encuentra en otra posición.
-* Devuelve true si todas son correctas
-**************************************************************/
-function comprobarPalabra() {
+function haGanado() {
     inputs = $('#word' + intentos).children();
     //Aquí alamacenamos la palabra de los inputs     
     var palabra = [];
@@ -47,9 +34,30 @@ function comprobarPalabra() {
     for (i = 0; i < 5; i++) {
         palabra += inputs.eq(i).val();
     }
-    //Verificamos si ha acertado. En tal caso termina la función
-    if(palabra == palabraBuscada) return true;
-    
+    //Verificamos si ha acertado. En tal caso informa al usuario y termina la función
+    if (palabra == palabraBuscada){
+        let mensajeGanador = '<h2 class="alert alert-success text-center" role="alert">Enhorabuena, has ganado';
+        mensajeGanador += '<br><button class="btn-success btn-sm" onclick="window.location.reload();">Volver a jugar</button></h2>';
+        $('#mensaje').append(mensajeGanador);
+        return true;
+    } 
+    else return false;
+}
+/*************************************************************
+* Comprueba cada letra y la colorea en verde si está en la posición correcta
+* y en amarillo si se encuentra en otra posición.
+* Devuelve true si todas son correctas
+**************************************************************/
+function colorearLetras() {
+    inputs = $('#word' + intentos).children();
+    //Aquí alamacenamos la palabra de los inputs     
+    var palabra = [];
+
+    // Obtenemos la palabra escrita por el usuario
+    for (i = 0; i < 5; i++) {
+        palabra += inputs.eq(i).val();
+    }
+
     //Recorre la palabra buscada comparando con cada caracter de la palara escrita
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
@@ -60,21 +68,31 @@ function comprobarPalabra() {
                     //Sale del bucle para que no tenga en cuenta esa letra si se repite más adelante
                     break; //Sale del bucle
                 }
-                else{   //Esta pero en otra posición
-                        inputs.eq(i).css('background-color', '#ffc107');
-                        break;
+                else {   //Esta pero en otra posición
+                    inputs.eq(i).css('background-color', '#ffc107');
+                    break;
                 }
             }
         }
     }
-    
+
 
 }
 /*******************************************************************
  * FUNCIÓN QUE ELIGE UNA PALABRA ALEATORIA DE LA LISTA DE PALABRAS
  */
-function palabraAleatoria(){
-    var rand = Math.floor(Math.random()*listaPalabras.length);
+function palabraAleatoria() {
+    var rand = Math.floor(Math.random() * listaPalabras.length);
     palabraBuscada = listaPalabras[rand];
-    console.log(palabraBuscada);    
+    console.log(palabraBuscada);
+}
+
+function haPerdido(){
+    //Deshabilita el último input y el botón pulsado
+    $('#word' + intentos).children().prop('disabled', true);
+    // Muestra un mensaje y un botón para volver a jugar
+    let mensajePerdedor = '<h2 class="alert alert-warning text-center" role="alert">Lo siento, no tienes más intentos';
+    mensajePerdedor += '<br><button class="btn-success btn-sm" onclick="window.location.reload();">Volver a jugar</button></h2>';
+    $('#mensaje').append(mensajePerdedor);
+              
 }
